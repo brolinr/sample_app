7cadcef59ab095445d08b6e33e52dc8cba3a5c2e
brolinr <brolinremz@gmail.com>
2021-06-04 00:42:41 +0200

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,6 +14,9 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    
+    log_out if logged_in?
+    redirect_to root_url
   end
+
 end
+
--- test/helpers/test_helper.rb
+++
@@ -1,9 +0,0 @@
-ENV['RAILS_ENV'] ||= 'test'
-
-class ActiveSupport::TestCase
-    fixtures :all
-
-    def is_logged_in?
-        !session[:user_id].nil?
-    end
-end
