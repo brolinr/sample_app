0e39effeab4490d47323319f8f59ae6dd62522cf
brolinr <brolinremz@gmail.com>
2021-06-07 15:14:02 +0200

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update, :edit, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+ 
   def new
     @user = User.new
   end
@@ -18,10 +22,30 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
+
+  end
+
   def destroy
-    log_out
-    redirect_to(root_url)
-  end    
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
     
 
   private
@@ -30,5 +54,26 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+
+    #Confirms a logged in user
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in to access this page."
+        redirect_to login_url
+      end
+    end
+
+    #Confirms the correct user
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless correct_user?(@user)
+    end
+      
+    # Confirms an admin user.
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+  
 
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -22,19 +22,6 @@ module SessionsHelper
             end
         end
     end
-    
-    # Returns the user corresponding to the remember token cookie.
-    def current_user
-        if (user_id = session[:user_id])
-            current_user ||= User.find_by(id: session[:user_id])
-        elsif (user_id = cookies.encrypted[:user_id])
-            user = User.find_by(id: user_id)
-            if user && user.authenticated?(cookies[:remember_token])
-                log_in user
-                @current_user = user
-            end
-        end
-    end
 
     def logged_in?
         !current_user.nil?
@@ -59,4 +46,19 @@ module SessionsHelper
         @current_user = nil
     end
  
+    def current_user?(user)
+        user && user == current_user
+    end
+
+    # Redirects to stored location (or to the default).
+    def redirect_back_or(default)
+        redirect_to(session[:forwarding_url] || default)
+        session.delete(:forwarding_url)
+    end
+
+    # Stores the URL trying to be accessed.
+    def store_location
+        session[:forwarding_url] = request.original_url if request.get?
+    end
+
 end
